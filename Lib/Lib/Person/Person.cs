ebd6869a2dafb5b989d89179f06d938ed152482a
Chris Xiong <yxiong@Chriss-Mac-mini.local>
2020-04-23 23:35:22 +0800

    Array

--- Lib/Lib/Person/Person.cs
+++ Lib/Lib/Person/Person.cs
@@ -3,8 +3,18 @@ namespace Lib.Person
 {
     public class Person
     {
-        public Person()
+        public DateTime Birthday { get; }
+        public string FirstName { get; }
+        public string LastName { get; }
+
+        public Person(string firstName, string lastName, DateTime birthday)
         {
+            FirstName = firstName;
+            LastName = lastName;
+            Birthday = birthday;
         }
+
+        public override string ToString() => $"{FirstName} {LastName}";
     }
+
 }
