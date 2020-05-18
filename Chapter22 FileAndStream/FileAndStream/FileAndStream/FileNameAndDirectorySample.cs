a873838b93fde934346d5631aa7f36d1a7905531
Chris Xiong <yxiong@Chriss-Mac-mini.local>
2020-05-19 01:33:09 +0800

    创建文件，写入文件

---
+++ Chapter22 FileAndStream/FileAndStream/FileAndStream/FileNameAndDirectorySample.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace FileAndStream
+{
+    public static class FileNameAndDirectorySample
+    {
+        const string SampleFileName = "Sample1.md";
+        public static void DisplaySample()
+        {
+            CreateFile();
+        }
+        static string GetDocumentFolder() =>
+            Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+
+        static void CreateFile()
+        {
+            string filename = Path.Combine(GetDocumentFolder(), SampleFileName);
+            //创建文本并写入数据
+            File.WriteAllText(filename, "Hello, World");
+        }
+    }
+}
