9da919b68203d57d4b7165fb84b2e777737dfc88
Chris Xiong <yxiong@Chriss-Mac-mini.local>
2020-05-20 02:26:25 +0800

    读写流，压缩文件，监听文件

---
+++ Chapter22 FileAndStream/FileAndStream/FileAndStream/StreamReaderAndStreamWriter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace FileAndStream
+{
+    public static class StreamReaderAndStreamWriter
+    {
+
+        static void ReadFileUsingReader(string filename)
+        {
+            var stream = new FileStream(filename, FileMode.Open, FileAccess.Read, FileShare.Read);
+            //StreamReader默认使用utf-8编码
+            //var reader = File.OpenText(filename) 也可以创建SteamReader
+            using(var reader = new StreamReader(stream)) //FileStream传入StreamReader创建StreamReader对象
+            {
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    Console.WriteLine(line);
+                }
+            }
+        }
+
+        public static void WriteFileUsingWriter(string fileName, string[] lines)
+        {
+            var outputStream = File.OpenWrite(fileName);
+            //StreamWriter默认使用utf-8编码
+            using (var writer = new StreamWriter(outputStream))
+            {
+                byte[] preamble = Encoding.UTF8.GetPreamble();
+                outputStream.Write(preamble, 0, preamble.Length);
+                writer.Write(lines);
+            }
+        }
+    }
+}
