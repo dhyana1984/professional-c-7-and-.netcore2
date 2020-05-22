abd45c90bb9f5f41922ee2f9fb7523852878186b
Chris Xiong <yxiong@Chriss-Mac-mini.local>
2020-05-22 18:08:05 +0800

    HttpClient,WebListener

---
+++ Chapter23 Network/NetworkSample/NetworkSample/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NetWorkSample;
+
+namespace NetworkSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpClientSample.DisplaySample();
+            Console.ReadLine();
+        }
+    }
+}
