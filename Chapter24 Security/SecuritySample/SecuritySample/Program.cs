e515f21b1065f9338f893c489fe86faaa8c478f9
Chris Xiong <yxiong@Chriss-Mac-mini.local>
2020-05-25 01:37:34 +0800

    Identity, Principal

---
+++ Chapter24 Security/SecuritySample/SecuritySample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SecuritySample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrincipalSample.DisplaySample();
+        }
+    }
+}
---
+++ Chapter24 Security/SecuritySample/SecuritySample/SigningDemo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace SecuritySample
+{
+    public static class SigningDemo
+    {
+        public static void DisplaySample()
+        {
+            Run();
+        }
+
+        private CngKey _aliceSignature;
+        private byte[] _alicePubKeyBlob;
+
+        private static void Run()
+        {
+
+        }
+
+    }
+}
