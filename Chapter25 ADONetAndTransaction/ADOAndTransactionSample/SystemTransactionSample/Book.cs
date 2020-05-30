230fab8143b1185dbf7eed67ffaeca4156e80b2a
Chris Xiong <yxiong@Chriss-Mac-mini.local>
2020-05-31 02:10:24 +0800

    事务

--- Chapter25 ADONetAndTransaction/ADOAndTransactionSample/SystemTransactionSample/Book.cs
+++ Chapter25 ADONetAndTransaction/ADOAndTransactionSample/SystemTransactionSample/Book.cs
@@ -3,8 +3,10 @@ namespace SystemTransactionSample
 {
     public class Book
     {
-        public Book()
-        {
-        }
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Publisher { get; set; }
+        public string Isbn { get; set; }
+        public DateTime? ReleaseDate { get; set; }
     }
 }
--- Chapter25 ADONetAndTransaction/ADOAndTransactionSample/SystemTransactionSample/BookData.cs
+++ Chapter25 ADONetAndTransaction/ADOAndTransactionSample/SystemTransactionSample/BookData.cs
@@ -1,10 +1,59 @@
 ﻿using System;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using System.Transactions;
+using Microsoft.Extensions.Configuration;
+
+
 namespace SystemTransactionSample
 {
     public class BookData
     {
-        public BookData()
+        const string connectionString = @"server=192.168.1.7\SQLEXPRESS; User Id=sa;Password=xiongyi1984; database=Books";
+
+        public async Task AddBookAsync(Book book, Transaction tx)
         {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string sql = "INSERT INTO [ProCSharp].[Books] ([Title], [Publisher], [Isbn], [ReleaseDate]) " +
+                    "VALUES (@Title, @Publisher, @Isbn, @ReleaseDate)";
+
+                await connection.OpenAsync();
+                if (tx != null)
+                {
+                    //使用connection.EnlistTransaction来调用System.Transaction
+                    //connection参与该事务的结果, 此处tx是CommittableTransaction
+                    connection.EnlistTransaction(tx);
+                }
+                var command = connection.CreateCommand();
+                command.CommandText = sql;
+                command.Parameters.AddWithValue("Title", book.Title);
+                command.Parameters.AddWithValue("Publisher", book.Publisher);
+                command.Parameters.AddWithValue("Isbn", book.Isbn);
+                command.Parameters.AddWithValue("ReleaseDate", book.ReleaseDate);
+
+                await command.ExecuteNonQueryAsync();
+            }
+        }
+
+        public void AddBook(Book book)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string sql = "INSERT INTO [ProCSharp].[Books] ([Title], [Publisher], [Isbn], [ReleaseDate]) " +
+                    "VALUES (@Title, @Publisher, @Isbn, @ReleaseDate)";
+
+                connection.Open();
+
+                var command = connection.CreateCommand();
+                command.CommandText = sql;
+                command.Parameters.AddWithValue("Title", book.Title);
+                command.Parameters.AddWithValue("Publisher", book.Publisher);
+                command.Parameters.AddWithValue("Isbn", book.Isbn);
+                command.Parameters.AddWithValue("ReleaseDate", book.ReleaseDate);
+
+                command.ExecuteNonQuery();
+            }
         }
     }
 }
