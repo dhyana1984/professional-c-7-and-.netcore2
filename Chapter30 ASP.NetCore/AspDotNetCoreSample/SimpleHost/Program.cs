65f132e9e9df6862fff5dbb6f3a27cfb0134767b
Chris Xiong <yxiong@Chriss-Mac-mini.local>
2020-06-10 01:39:10 +0800

    ASP.Net Core

---
+++ Chapter30 ASP.NetCore/AspDotNetCoreSample/SimpleHost/Program.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace SimpleHost
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //context从客户端读取请求并返回内容
+            WebHost.Start(async context =>
+            {
+                await context.Response.WriteAsync("<h1>A Simple Host!</h1>");
+            }).WaitForShutdown();
+        }
+    }
+}
