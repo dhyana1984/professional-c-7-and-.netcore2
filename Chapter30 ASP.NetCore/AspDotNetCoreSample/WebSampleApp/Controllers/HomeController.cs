56f06772331910f6049e45d1e5a3a55e5818c0e2
Chris Xiong <yxiong@Chriss-Mac-mini.local>
2020-06-11 11:31:50 +0800

    ASP.NET Core

---
+++ Chapter30 ASP.NetCore/AspDotNetCoreSample/WebSampleApp/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace WebSampleApp.Controllers
+{
+    public class HomeController
+    {
+        private readonly ISampleService _service;
+        public HomeController(ISampleService service)
+        {
+            _service = service;
+        }
+
+        public async Task Index(HttpContext context)
+        {
+            var sb = new StringBuilder();
+            sb.Append("<ul>");
+            sb.Append(string.Join(string.Empty,
+              _service.GetSampleStrings().Select(s => s.Li()).ToArray()));
+            sb.Append("</ul>");
+            context.Response.StatusCode = 200;
+            await context.Response.WriteAsync(sb.ToString());
+        }
+    }
+}
