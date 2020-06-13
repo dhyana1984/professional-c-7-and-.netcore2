b2e690ded3dcae2a20d54eb99988bbedadace90e
Chris Xiong <yxiong@Chriss-Mac-mini.local>
2020-06-13 23:50:21 +0800

    .net core MVC

---
+++ Chapter31 MVC/MVCSampleApp/MVCSampleApp/MVCSampleApp/Controllers/ResultController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVCSampleApp.Models;
+
+namespace MVCSampleApp.Controllers
+{
+    public class ResultController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult JsonDemo()
+        {
+            var m = new Menu
+            {
+                Id = 3,
+                Text = "Grilled sausage with sauerkraut and potatoes",
+                Price = 12.90,
+                Date = new DateTime(2018, 3, 31),
+                Category = "Main"
+            };
+            return Json(m);
+        }
+
+        //返回一个VirtualFileResult，显示在页面上
+        public IActionResult FileDemo() =>
+            File("~/images/timg.jpeg", "image/jpeg");
+    }
+}
---
+++ Chapter31 MVC/MVCSampleApp/MVCSampleApp/MVCSampleApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVCSampleApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
