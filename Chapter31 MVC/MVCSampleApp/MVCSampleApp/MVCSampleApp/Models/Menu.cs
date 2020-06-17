e8b265d76adfdc7393ea00ebdff2a8ac7c6b29e4
Chris Xiong <yxiong@Chriss-Mac-mini.local>
2020-06-17 23:25:30 +0800

    .net MVC core

--- Chapter31 MVC/MVCSampleApp/MVCSampleApp/MVCSampleApp/Models/Menu.cs
+++ Chapter31 MVC/MVCSampleApp/MVCSampleApp/MVCSampleApp/Models/Menu.cs
@@ -6,12 +6,18 @@ namespace MVCSampleApp.Models
     public partial class Menu
     {
         public int Id { get; set; }
+
+        [Display(Name ="Menu")]
         [Required, StringLength(50)]
         public string Text { get; set; }
+
+
         [Display(Name = "Price"), DisplayFormat(DataFormatString = "{0:c}")]
         public double Price { get; set; }
-        [DataType(DataType.Date)]
+
+        [DataType(DataType.Date)] //这个注解表示显示为日期格式
         public DateTime Date { get; set; }
+
         [StringLength(10)]
         public string Category { get; set; }
     }
