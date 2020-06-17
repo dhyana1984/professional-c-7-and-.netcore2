5570ea36a790267847cd58d97f25348e3adf9dbf
Chris Xiong <yxiong@Chriss-Mac-mini.local>
2020-06-18 00:04:12 +0800

    .net Core MVC

---
+++ Chapter31 MVC/MVCSampleApp/MVCSampleApp/MVCSampleApp/Controllers/TagHelperController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MVCSampleApp.Controllers
+{
+    public class TagHelperController : Controller
+    {
+        public IActionResult CustomTable() => View(GetSampleMenus());
+        private IList<Menu> GetSampleMenus() =>
+            new List<Menu>
+            {
+                new Menu
+                {
+                    Id = 1,
+                    Text = "Schweinsbraten mit Knödel und Sauerkraut",
+                    Price = 8.5,
+                    Date = new DateTime(2018, 10, 5),
+                    Category = "Main"
+                },
+                new Menu
+                {
+                    Id = 2,
+                    Text = "Erdäpfelgulasch mit Tofu und Gebäck",
+                    Price = 8.5,
+                    Date = new DateTime(2018, 10, 6),
+                    Category = "Vegetarian"
+                },
+                new Menu
+                {
+                    Id = 3,
+                    Text = "Tiroler Bauerngröst'l mit Spiegelei und Krautsalat",
+                    Price = 8.5,
+                    Date = new DateTime(2018, 10, 7),
+                    Category = "Vegetarian"
+                }
+            };
+    }
+}
